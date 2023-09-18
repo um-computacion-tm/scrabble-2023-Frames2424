25ed87889544c2229737ad80c154ff9084d375e5
frames2424 <frames2424@gmail.com>
2023-09-18 15:27:41 -0300

    fix test

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,29 @@
-def testPuntajes_II(self):
+import unittest
+from game.models import (
+    BagTiles,
+    Tile,
+)
+from unittest.mock import patch
+
+
+class TestTiles(unittest.TestCase):
+    def test_tile(self):
+        tile = Tile('A', 1)
+        self.assertEqual(tile.letter, 'A')
+        self.assertEqual(tile.value, 1)
+
+    def test_tile_II(self):
+        tile = Tile('B', 2)
+        self.assertEqual(tile.letter, 'B')
+        self.assertEqual(tile.value, 2)
+
+
+class TestBagTiles(unittest.TestCase):
+    def testPuntajes_I(self):
+        values = BagTiles().tiles
+        self.assertEqual(values['A'], 1)
+
+    def testPuntajes_II(self):
         values = BagTiles().tiles
         self.assertEqual(values['Z'], 10)
     # @patch('random.shuffle')
@@ -43,3 +68,4 @@ def testPuntajes_II(self):
 
 
 if __name__ == '__main__':
+    unittest.main()
