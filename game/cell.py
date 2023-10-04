248e4911063a0c77ea8ab9e00e9d2ec39872a7f0
Frames2424 <frames2424@gmail.com>
2023-10-03 23:14:55 -0300

    cell.py

--- game/cell.py
+++ game/cell.py
@@ -1,16 +1,3 @@
 from game.models import Tile
 from game.board import Board
 
-
-# class Cell(Board):
-    
-#     def returnPointsAndMultiplier(self):
-#         simple = sum(self.wordPoints)
-#         if self.condicionCero == True:
-#             return simple * 2
-        
-#         if self.condicionUno == True:
-#             return simple * 3
-        
-#         else: 
-#             return simple
