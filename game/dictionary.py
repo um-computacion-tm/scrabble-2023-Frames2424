2fd17359e810687e0e5e4efdd21ada476b82565e
frames2424 <frames2424@gmail.com>
2023-10-15 23:38:01 -0300

    dictionary refactoriced

--- game/dictionary.py
+++ game/dictionary.py
@@ -1,11 +1,12 @@
-
-
 class Dictionary:
-    
-    def isValid(self, validate):
-        if validate == 'SI':
-            return True
-        elif validate == 'NO':
-            return False
-        else: 
-            return None
+    def __init__(self, file_path):
+        self.words = self.words_load(file_path)
+
+    @staticmethod
+    def words_load(file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return set(word.strip() for word in file)
+
+    def word_exist(self, word):
+        word = word.lower()
+        return word in self.words
