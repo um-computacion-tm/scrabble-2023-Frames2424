a3ddf5a21caaddbef0550504f756ec303e90ecf5
frames2424 <frames2424@gmail.com>
2023-10-18 21:47:02 -0300

    cell nows works

--- game/cell.py
+++ game/cell.py
@@ -1,3 +1,43 @@
-from game.models import Tile
-from game.board import Board
+from game.Tiles import Tile
 
+class Cell:
+    def __init__(self, multiplier, multiplier_type, letter, active):
+        self.multiplier = multiplier
+        self.multiplier_type = multiplier_type
+        self.letter = letter
+        self.active = active
+    
+    def __repr__(self):
+        if self.letter != None:
+            return f"[ {self.letter.letter} ]"    
+        else:
+            if self.multiplier == 1:
+                return f"[   ]"
+            else:
+                return f"[{self.multiplier_type}x{self.multiplier}]"
+
+    def add_letter(self, letter:Tile):
+        self.letter = letter 
+
+    def calculate_value(self):
+        if self.letter is None:
+            return 0
+        if self.multiplier_type == 'L' and self.active == True:
+            return self.letter.value * self.multiplier
+        else:
+            return self.letter.value
+
+class calculate_word_value:
+    def __init__(self,word):
+        self.word = word
+        
+    def calculate_word(self):
+        values = 0
+        for cell in self.word:
+            values += cell.calculate_value()
+        for cell in self.word:
+            if cell.multiplier_type == 'W' and cell.active == True:
+                values = values * cell.multiplier
+
+        return values
+        
